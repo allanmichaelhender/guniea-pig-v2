a83c908af9ddbb720a16d557c4fb90453daaf863
Allan <allanhender@gmail.com>
2026-04-02 13:47:03 +0100

    moving to certbot

--- backend/guinea_pig/settings.py
+++ backend/guinea_pig/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = [
     "nginx",
     "16.16.216.22",
     "13.49.100.49",
-    "https://guinea-pig-portfolio.ddnsfree.com/"
+    "guinea-pig-portfolio.ddnsfree.com",
 ]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
@@ -75,7 +75,13 @@ MIDDLEWARE = [
 # Since Nginx proxies both to port 80, we can often simplify or remove CORS
 # For now, allow the proxy host
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://127.0.0.1", "http://16.16.216.22", "http://13.49.100.49", "https://guinea-pig-portfolio.ddnsfree.com/"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://16.16.216.22",
+    "http://13.49.100.49",
+    "https://guinea-pig-portfolio.ddnsfree.com/",
+]
 
 ROOT_URLCONF = "guinea_pig.urls"
 
